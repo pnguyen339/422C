bb413b740bd8cf0ccf0fa0cdc3b38f9eeab31459
Peter Nguyen <pnguyen339@gmail.comi>
2017-02-17 15:41:06 -0600

    add quizzes

---
+++ Quizzes/Header.java
@@ -0,0 +1,9 @@
+// File name here
+/*
+ * EE422C Quiz 1 submission by
+ * Replace <...> with your actual data.
+ * <Student Name>
+ * <Student EID>
+ * <5-digit Unique No.>
+ * Fall 2016
+ */
---
+++ Quizzes/HelloWorld.java
@@ -0,0 +1,25 @@
+
+
+public class HelloWorld{
+
+	/*
+	 * This method print out hello world
+	 */	
+	public static void  main(String[] args){
+		hello();
+
+	}
+
+
+
+
+	/*
+	 * This method print out hello world
+	 */
+
+
+	private static void hello(){
+		System.out.println("Hello World!\n");
+	}
+
+}
